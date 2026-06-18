a7b010c0812d02096c10fc38dcb9c517f4bfcb3c
apenchuu <steven41@ui.ac.id>
2026-06-18 23:49:49 +0700

    commit aj

--- stonefish_bluerov2/scripts/ardusim_patch.py
+++ stonefish_bluerov2/scripts/ardusim_patch.py
@@ -52,8 +52,8 @@ class Patch(Node):
         self.gps = None
         self.odom = None
 
-        # self.gps_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
-        # self.gps_addr = ("127.0.0.1", 25100)
+        self.gps_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
+        self.gps_addr = ("127.0.0.1", 25100)
 
     def _imu_callback(self, msg):
         self.imu = msg
@@ -65,7 +65,7 @@ class Patch(Node):
         self.odom = msg
 
     def looper(self):
-        if self.imu is None or self.odom is None:
+        if self.imu is None or self.odom is None or self.gps is None:
             self.get_logger().info("Wating for callbacks", once=False)
             time.sleep(.5)
             return
@@ -196,29 +196,29 @@ class Patch(Node):
 
         # print(self.gps.latitude)
 
-        # gps_data = {
-        #         'time_usec' : int(c_time/1e3),                        # (uint64_t) Timestamp (micros since boot or Unix epoch)
-        #         'gps_id' : 0,                           # (uint8_t) ID of the GPS for multiple GPS inputs
-        #         # 'ignore_flags' : 8,                     # (uint16_t) Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
-        #         # 'time_week_ms' : 0,                     # (uint32_t) GPS time (milliseconds from start of GPS week)
-        #         # 'time_week' : 0,                        # (uint16_t) GPS week number
-        #         # 'fix_type' : 3,                         # (uint8_t) 0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
-        #         'lat' : int(self.gps.latitude*1e7),                              # (int32_t) Latitude (WGS84), in degrees * 1E7
-        #         'lon' : int(self.gps.longitude*1e7),                              # (int32_t) Longitude (WGS84), in degrees * 1E7
-        #         'alt' : 0,                              # (float) Altitude (AMSL, not WGS84), in m (positive for up)
-        #         # 'hdop' : 1,                             # (float) GPS HDOP horizontal dilution of position in m
-        #         # 'vdop' : 1,                             # (float) GPS VDOP vertical dilution of position in m
-        #         # 'vn' : 0,                               # (float) GPS velocity in m/s in NORTH direction in earth-fixed NED frame
-        #         # 've' : 0,                               # (float) GPS velocity in m/s in EAST direction in earth-fixed NED frame
-        #         # 'vd' : 0,                               # (float) GPS velocity in m/s in DOWN direction in earth-fixed NED frame
-        #         # 'speed_accuracy' : 0,                   # (float) GPS speed accuracy in m/s
-        #         # 'horiz_accuracy' : 0,                   # (float) GPS horizontal accuracy in m
-        #         # 'vert_accuracy' : 0,                    # (float) GPS vertical accuracy in m
-        #         # 'satellites_visible' : 7                # (uint8_t) Number of satellites visible.
-        # }
+        gps_data = {
+                'time_usec' : int(c_time/1e3),                        # (uint64_t) Timestamp (micros since boot or Unix epoch)
+                'gps_id' : 0,                           # (uint8_t) ID of the GPS for multiple GPS inputs
+                # 'ignore_flags' : 8,                     # (uint16_t) Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
+                # 'time_week_ms' : 0,                     # (uint32_t) GPS time (milliseconds from start of GPS week)
+                # 'time_week' : 0,                        # (uint16_t) GPS week number
+                # 'fix_type' : 3,                         # (uint8_t) 0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
+                'lat' : int(self.gps.latitude*1e7),                              # (int32_t) Latitude (WGS84), in degrees * 1E7
+                'lon' : int(self.gps.longitude*1e7),                              # (int32_t) Longitude (WGS84), in degrees * 1E7
+                'alt' : 0,                              # (float) Altitude (AMSL, not WGS84), in m (positive for up)
+                # 'hdop' : 1,                             # (float) GPS HDOP horizontal dilution of position in m
+                # 'vdop' : 1,                             # (float) GPS VDOP vertical dilution of position in m
+                # 'vn' : 0,                               # (float) GPS velocity in m/s in NORTH direction in earth-fixed NED frame
+                # 've' : 0,                               # (float) GPS velocity in m/s in EAST direction in earth-fixed NED frame
+                # 'vd' : 0,                               # (float) GPS velocity in m/s in DOWN direction in earth-fixed NED frame
+                # 'speed_accuracy' : 0,                   # (float) GPS speed accuracy in m/s
+                # 'horiz_accuracy' : 0,                   # (float) GPS horizontal accuracy in m
+                # 'vert_accuracy' : 0,                    # (float) GPS vertical accuracy in m
+                # 'satellites_visible' : 7                # (uint8_t) Number of satellites visible.
+        }
 
-        # gps_data = json.dumps(gps_data)
-        # self.gps_sock.sendto(gps_data.encode(), ("127.0.0.1", 25100))
+        gps_data = json.dumps(gps_data)
+        self.gps_sock.sendto(gps_data.encode(), ("127.0.0.1", 25100))
 
 def main(args=None):
     rclpy.init(args=args)
